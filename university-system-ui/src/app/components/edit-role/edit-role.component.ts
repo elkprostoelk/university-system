51763deb4735e9359267aaa71fd000ec5ff56fd9
elkprostoelk <55785891+elkprostoelk@users.noreply.github.com>
2022-03-18 22:43:29 +0200

    added edit role component

---
+++ university-system-ui/src/app/components/edit-role/edit-role.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {RoleService} from "../../services/role/role.service";
+import {RoleDto} from "../../models/roleDto";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../services/user/user.service";
+
+@Component({
+  selector: 'app-edit-role',
+  templateUrl: './edit-role.component.html',
+  styleUrls: ['./edit-role.component.css']
+})
+export class EditRoleComponent implements OnInit {
+
+  id: number | undefined;
+  role$?: Observable<RoleDto>;
+  editRoleForm: FormGroup
+  constructor(private builder: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private roleService: RoleService) {
+    this.editRoleForm = this.builder.group({
+      name: ['', Validators.required]
+    });
+    this.activatedRoute.params
+      .subscribe(params => {
+        this.id = params['id'];
+        if (this.id) {
+          this.role$ = this.roleService.getRole(this.id);
+        }
+      });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  deleteUserFromRole(roleId: number | undefined, userId: number): void {
+    if (confirm('Are you sure to delete user from this role?') && roleId) {
+      this.userService.deleteUserFromRole(userId, roleId)
+        .subscribe();
+    }
+  }
+
+  editRole(value: any): void {
+    if (this.id) {
+      this.roleService.editRole(this.id, value)
+        .subscribe();
+      this.router.navigateByUrl('/admin');
+    }
+  }
+}
